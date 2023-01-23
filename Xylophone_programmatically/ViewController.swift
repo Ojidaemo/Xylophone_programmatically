34dc746ec6b437357536ff9c256c13f448558d0d
Ojidaemo <vit.martsinovich@gmail.com>
2023-01-23 15:52:31 +0300

    Initial Commit

---
+++ Xylophone_programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xylophone_programmatically
+//
+//  Created by Vitali Martsinovich on 2023-01-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
