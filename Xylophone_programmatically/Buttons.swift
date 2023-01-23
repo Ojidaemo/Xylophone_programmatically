0d79bfb14d748f22897f1a6cd21d977ebb72a38f
Ojidaemo <vit.martsinovich@gmail.com>
2023-01-23 21:43:04 +0300

    Add buttons and sounds

---
+++ Xylophone_programmatically/Buttons.swift
@@ -0,0 +1,13 @@
+//
+//  Buttons.swift
+//  Xylophone_programmatically
+//
+//  Created by Vitali Martsinovich on 2023-01-23.
+//
+
+import Foundation
+import UIKit
+
+class Buttons: UIView {
+    
+}
--- Xylophone_programmatically/ViewController.swift
+++ Xylophone_programmatically/ViewController.swift
@@ -6,14 +6,174 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
+    let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.distribution = .fillEqually
+        stack.spacing = 5
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    var buttonC: UIButton = {
+       
+        let button = UIButton()
+        button.setTitle("C", for: .normal)
+        button.backgroundColor = .red
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    var buttonD: UIButton = {
+       
+        let button = UIButton()
+        button.setTitle("D", for: .normal)
+        button.backgroundColor = .systemOrange
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    var buttonE: UIButton = {
+       
+        let button = UIButton()
+        button.setTitle("E", for: .normal)
+        button.backgroundColor = .systemYellow
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    var buttonF: UIButton = {
+       
+        let button = UIButton()
+        button.setTitle("F", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    var buttonG: UIButton = {
+       
+        let button = UIButton()
+        button.setTitle("G", for: .normal)
+        button.backgroundColor = .systemIndigo
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    var buttonA: UIButton = {
+       
+        let button = UIButton()
+        button.setTitle("A", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    var buttonB: UIButton = {
+       
+        let button = UIButton()
+        button.setTitle("B", for: .normal)
+        button.backgroundColor = .systemPurple
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(stackView)
+        addButtonsToStack()
+        setConsrtaints()
+        
     }
+    
+    func addButtonsToStack() {
+        
+        stackView.addArrangedSubview(buttonC)
+        stackView.addArrangedSubview(buttonD)
+        stackView.addArrangedSubview(buttonE)
+        stackView.addArrangedSubview(buttonF)
+        stackView.addArrangedSubview(buttonG)
+        stackView.addArrangedSubview(buttonA)
+        stackView.addArrangedSubview(buttonB)
+    }
+    
+    
+    func playSound(soundName: String) {
+        
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
+    @objc func buttonPressed(sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
 
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            sender.alpha = 1.0
+        }
+    }
+    
+    
+    func setConsrtaints() {
+        
+        NSLayoutConstraint.activate([
+            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        
+        ])
+    }
+    
 
 }
 
